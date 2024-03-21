71db2ba9133d1222e181fcf347ac36521ce9171c
devshadow <bozkurt02cenap@gmail.com>
2024-03-21 21:35:51 +0300

    abaout

--- pages/about.jsx
+++ pages/about.jsx
@@ -29,7 +29,7 @@ export default function About() {
   return (
     <div className="px-10 sm:px-20 md:px-32 lg:mb-12 lg:px-60 mx-auto max-w-[75rem]">
       <Head>
-        <title>about ✦ pranjal</title>
+        <title>Hakkımda ✦ Compust</title>
         <link rel="icon" href="/Avatar-white.svg" />
       </Head>
       <main className="  max-w-screen ">
@@ -38,7 +38,7 @@ export default function About() {
             <h1
               className={`mt-6 mb-12 bg-gradient-to-br to-blue-500 via-red-400/90 from-yellow-500  bg-clip-text text-transparent selection:text-gray-700 dark:selection:text-white/90 items-center mx-auto text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold`}
             >
-              About
+              Hakkımda
             </h1>
             <div className="mx-auto transition-all duration-1000 ease-in-out -mt-8">
               <Image
@@ -51,7 +51,7 @@ export default function About() {
               />
             </div>
             <p className="mt-8 max-w-xl text-center md:w-[70%] mx-auto text-sm md:text-base lg:text-xl mb-2 px-2">
-              Little about me. Also, It&apos;s not that hard to find me.
+             Ben Cenap, Web tasarım ile ilgileniyorum aynı zamanda uçakçıyım.
             </p>
             <div className="select-none flex justify-center space-x-4 items-center mt-2 text-gray-400">
               <svg
@@ -78,8 +78,8 @@ export default function About() {
           <CategoryCard />
           <div className=" text-[0.92rem] text-base mx-12 font-light dark:text-white text-gray-900">
             <p className="mt-6 mb-12 font-normal">
-              Front-end Engineer who develops modern, reactive, and
-              user-friendly web applications using the latest technologies.{" "}
+            20 yaşındayım ve şu anda Kültür Üniversitesi'nde Uçuş Harekat Yöneticiliği, 
+            Atatürk Üniversitesi'nde ise Sivil Hava Ulaştırma İşletmeciliği bölümlerinde eğitim alıyorum. Web alanında uzmanlık belgem bulunuyor.<br></br>{" "}
               <span className="inline-flex items-baseline">
                 <Image
                   src={Rocket}
@@ -87,9 +87,7 @@ export default function About() {
                   className="self-center w-5 h-5 mx-1"
                 />
               </span>{" "}
-              Believe a perfect blend of UI architecture is one, where the goals
-              and needs are accounted for in an elegant, efficient, and robust
-              design of the user interface{" "}
+              profesyonel anlamda ve hobi olarak birçok web sitesi kurma deneyimim var. <br></br>{" "}
               <span className="inline-flex items-baseline">
                 <Image
                   src={Sparkles}
@@ -97,11 +95,8 @@ export default function About() {
                   className="self-center w-5 h-5 mx-1"
                 />
               </span>{" "}
-              Experience in React.js, Next.js, and state management like
-              Redux.js with proficiency in web technologies like JavaScript,
-              Typescript, Tailwind CSS and SASS. building large-scale web
-              applications, cross-browser platforms, and design constraints on
-              the web, and building responsive user interfaces.{" "}
+              Havacılık ilgili eğitimime ek olarak, web dünyasında kendimi geliştirerek harçlığımı çıkarmak amacıyla freelancer olarak çalışmaya başladım. 
+              Yaratıcı çözümler üretmek, benim için sadece bir iş değil aynı zamanda bir hobi. Etkileyici bir web site arıyorsanız Bionluk profilime göz atmayı unutmayınız.<br></br>{" "}
               <span className="inline-flex items-baseline">
                 <Image
                   src={Flash}
@@ -112,38 +107,37 @@ export default function About() {
             </p>
 
             <p className="text-gray-400 select-none font-semibold mb-1 dark:text-white/30 ">
-              Contact
+              İletişim
             </p>
 
             <p className="">
-              While I may not be a widely recognized figure, i am readily
-              available for connection on my social media platforms. <br />
-              You can contact me on the{" "}
-              <span className="text-red-700 font-bold">GMail</span>{" "}
+            Geri bildiriminizi, sorularınızı ve önerilerinizi memnuniyetle karşılıyorum. 
+            Eğer belirli bir sorunuz veya yorumunuz varsa, lütfen bana doğrudan şu e-posta adresinden ulaşın:<br></br>{" "}
+              <span className="text-red-700 font-bold">Gmail</span>{" "}
               <a
                 onClick={() =>
-                  navigator.clipboard.writeText("shikhar.pranjal3@gmail.com")
+                  navigator.clipboard.writeText("cenapbozkurtt@gmail.com")
                 }
                 className=" underline underline-offset-4 decoration-2 decoration-purple-400 group select-all font-semibold dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer select "
-                href="mailto:shikhar.pranjal3@gmail.com"
+                href="mailto:cenapbozkurtt@gmail.com"
               >
                 {" "}
-                shikhar.pranjal3@gmail.com
-              </a>{" "}
-              or <span className="text-blue-700 font-bold">LinkedIn</span>{" "}
+                cenapbozkurtt@gmail.com
+              </a>{" "}<br></br>
+              {" "} <span className="text-blue-700 font-bold">LinkedIn</span>{" "}
               <a
-                href="http://linkedin.com/in/pranjalshikhar"
+                href="http://linkedin.com/in/cenapbozkurtt"
                 className=" underline underline-offset-4 decoration-2 decoration-purple-400 font-semibold dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer select"
                 rel="noopener noreferrer"
                 target="blank"
               >
                 {" "}
-                Pranjal Shikhar Sinha{" "}
-              </a>{" "}
-              or you can DM me on{" "}
+                Cenap Bozkurt{" "}
+              </a>{" "}<br></br>
+              Instagram hesabımından direkt mesaj atabilirsiniz.{" "}
               <span className="text-orange-500 font-bold">Instagram</span>{" "}
               <a
-                href="https://instagram.com/pranjal.fig"
+                href="https://instagram.com/compust"
                 target="blank"
                 rel="noopener noreferrer"
                 className=" font-semibold underline underline-offset-4 decoration-2 decoration-purple-400 dark:hover:text-purple-400 hover:text-purple-700 cursor-pointer select "
@@ -155,7 +149,7 @@ export default function About() {
           </div>
           <div className="flex mx-auto justify-center mt-16 select-none">
             <span className="font-black font-app text-2xl md:text-3xl align-text-bottom tracking-wider mx-3 md:mx-6">
-              dattebayo
+              compust | c3n4p
             </span>
             {currentTheme === "dark" ? (
               <Image src={love_white} alt="love" width="30" className="-mt-4" />
